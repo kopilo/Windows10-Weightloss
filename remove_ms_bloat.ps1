71cc8293759cd4409e2d94bc9a7a0a961368194e
kopilo hallard <kopilo.hallard@gmail.com>
2017-07-12 15:00:53 +1000

    Update remove_ms_bloat.ps1

--- remove_ms_bloat.ps1
+++ remove_ms_bloat.ps1
@@ -7,6 +7,5 @@ Foreach ($App in $Apps)
 {
     $App_n = Get-AppxPackage | Where-Object {$_.Name -eq $app}
     echo $App_n.PackageFullName
-    #$packageName = $App_n.PackageName
     Remove-AppxPackage $App_n.PackageFullName
 } 
